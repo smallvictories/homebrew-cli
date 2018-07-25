48264ed3ad35549270739cc9017af985d02bfe7c
Michael Dijkstra <micdijkstra@gmail.com>
2018-07-25 20:47:37 +1000

    Initial commit

---
+++ Formula/sv.rb
@@ -0,0 +1,12 @@
+# Homebrew Formula class for Small Victories
+class SmallVictories < Formula
+  desc 'Small Victories command line tool'
+  homepage 'https://www.smallvictori.es'
+  url 'https://github.com/smallvictories/sv-cli/archive/0.1.0.zip'
+  version '0.1.0'
+  sha256 'f7368d1620e8657a3404bce003435a2a950d678d9ad4b4b8246169ce619fc041'
+
+  def install
+    bin.install 'sv'
+  end
+end
---
+++ README.md
@@ -0,0 +1,8 @@
+# Homebrew Tap for sv cli
+
+## Usage
+
+```
+$ > brew tap sv/cli
+$ > brew install sv
+```
